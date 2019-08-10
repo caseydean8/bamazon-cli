3df878a4d06488a01e5c548e38ba2c82d967c13f
Casey Carroll <caseydean8@gmail.com>
2019-08-10 15:08:05 -0400

    Shows user purchase

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql');
 
+const inquirer = require('inquirer');
+
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -14,10 +16,56 @@ connection.connect(function(err) {
     afterConnect();
 });
 
+let quantity = 0;
+
 function afterConnect() {
     connection.query("select * from products", function(err, res) {
         if (err) throw err;
-        console.log(res);
+        for (let i = 0; i < res.length; i++)
+        console.log(res[i].product_name + "\n" + "$" + res[i].price + "\n" + "ID# " + res[i].item_id + "\n" + "------------------------------------------------");
+        inquirer
+        .prompt([
+            {
+                name: 'item_id',
+                type: 'input',
+                message: "Please select an item by id number"
+            },
+            {
+                name: 'quantity',
+                type: 'input',
+                message: "How many of these fantastic items would you like to buy my great friend?"
+            }
+        ])
+        .then(function(answer) {
+            let prodId = answer.item_id;
+            console.log(res[prodId - 1].product_name);
+        })
         connection.end();
+        // buyItem();
     })
 }
+
+// function buyItem() {
+//     inquirer
+//         .prompt([
+//             {
+//                 name: 'item_id',
+//                 type: 'input',
+//                 message: "Please select an item by id number"
+//             },
+//             {
+//                 name: 'quantity',
+//                 type: 'input',
+//                 message: "How many of these fantastic items would you like to buy my great friend?"
+//             }
+//         ])
+//         .then(function(answer) {
+//             // connection.query("select * from products", function(err, res) {
+//                 // if (err) throw err;
+//                 for (let i = 0; i < res.length; i++);
+//                 if (answer.item_id === res[i].item_id)
+//                 console.log(res[i].product_name);
+//             })
+//         // })
+// }
+
