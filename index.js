e57d7f1eb5662dc72cb76036c7a093c0406f6f6c
Casey Carroll <caseydean8@gmail.com>
2019-08-10 18:00:08 -0400

    Updates quantity to server.

--- index.js
+++ index.js
@@ -11,39 +11,59 @@ const connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-    if(err) throw err;
+    if (err) throw err;
     console.log("connected as id " + connection.threadId);
     afterConnect();
 });
+
+let quantityDesired = 0;
+let updateId = 0;
 
 function afterConnect() {
     connection.query("select * from products", function(err, res) {
         if (err) throw err;
         for (let i = 0; i < res.length; i++)
-        console.log(res[i].product_name + "\n" + "$" + res[i].price + "\n" + "ID# " + res[i].item_id + "\n" + "------------------------------------------------");
+            console.log(res[i].product_name + "\n" + "$" + res[i].price + "\n" + "ID# " + res[i].item_id + "\n" + "------------------------------------------------");
         inquirer
-        .prompt([
-            {
-                name: 'item_id',
-                type: 'input',
-                message: "Please select an item by id number"
-            },
-            {
-                name: 'quantity',
-                type: 'input',
-                message: "How many of these fantastic items would you like to buy my great friend?"
-            }
-        ])
-        .then(function(answer) {
-            let prodId = answer.item_id;
-            if (answer.quantity <= res[prodId - 1].stock_quantity) {
-            console.log(res[prodId - 1].product_name);
-            } else {
-                console.log("too many");
-            }
-        })
-        connection.end();
+            .prompt([{
+                    name: 'item_id',
+                    type: 'input',
+                    message: "Please select an item by id number"
+                },
+                {
+                    name: 'quantity',
+                    type: 'input',
+                    message: "How many of these fantastic items would you like to buy my great friend?"
+                }
+            ])
+            .then(function(answer) {
+                if (answer.item_id <= 10 && answer.item_id > 0) {
+                    let prodId = parseInt(answer.item_id) - 1;
+                    // console.log(prodId);
+                    updateId = parseInt(res[prodId].item_id);
+                    // console.log(updateId);
+                    quantityDesired = answer.quantity;
+                    // console.log(quantityDesired);
+                    let stockQuantity = res[prodId].stock_quantity;
+                    if (quantityDesired <= stockQuantity) {
+                        let total = (quantityDesired * res[prodId].price).toFixed(2);
+                        console.log(`${quantityDesired} X ${res[prodId].product_name} will cost $${total}`);
+                        updateQuantity();
+                    } else {
+                        console.log(`Sorry, we only have ${stockQuantity} in stock`);
+                        connection.end();
+                    }
+                } else {
+                    console.log("What is wrong with you? You really couldn't pick an ID number between 1 and 10? Get out of here!");
+                    connection.end();
+                }
+            })
     })
 }
 
-
+function updateQuantity() {
+    connection.query(`update products set stock_quantity = stock_quantity - ${quantityDesired} where item_id = ${updateId}`, function(err, res) {
+        if (err) throw err;
+    })
+    connection.end();
+}
