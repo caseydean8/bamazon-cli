3c139ffa5cbbd03b662bb2484b3ec6049669e344
Casey Carroll <caseydean8@gmail.com>
2019-08-13 10:44:44 -0400

    Shows table in terminal.

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -18,7 +18,7 @@ connection.connect(function(err) {
 })
 
 const supervisorMenu = () => {
-    let data,
+    let config, data,
     output;
 
     inquirer
@@ -39,17 +39,27 @@ const supervisorMenu = () => {
             group by departments.department_id
             `, function(err, res) {
                 if (err) throw err
-                let tableHeader = [["Department ID", "Department Name", "Overhead Costs", "Product Sales"]]
-                console.log(table(tableHeader))
 
-                for (let i = 0; i < res.length; i++){
-                data = [[res[i].department_id, res[i].department_name, res[i].overhead_costs, res[i].product_sales]]
+                data = [
+                    ["Dept \n ID", "Department \n Name", "Overhead \n Costs", "Product \n Sales"],
+                    
+                    [res[0].department_id, res[0].department_name, res[0].overhead_costs, res[0].product_sales],
+
+                    [res[1].department_id, res[1].department_name, res[1].overhead_costs, res[1].product_sales],
+
+                    [res[2].department_id, res[2].department_name, res[2].overhead_costs, res[2].product_sales],
+                    
+                    [res[3].department_id, res[3].department_name, res[3].overhead_costs, res[3].product_sales],
+                    
+                    [res[4].department_id, res[4].department_name, res[4].overhead_costs, res[4].product_sales],
+                    
+                    [res[5].department_id, res[5].department_name, res[5].overhead_costs, res[5].product_sales],
+                ]
                   
                 output = table(data)
-                console.log(output)
-                }
                 
-            })
+                console.log(output)
+                })
             } else {
                 console.log("Create new apartment")
             }
