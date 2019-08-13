8254e05eb7a26b87736b3c9fa92e883022ab3fa8
Casey Carroll <caseydean8@gmail.com>
2019-08-13 09:55:52 -0400

    Returns manager table info from sql database

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql')
 
-const inquirer = require('requirer')
+const inquirer = require('inquirer')
 
-const table = require('table')
+const {table} = require('table')
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -18,6 +18,9 @@ connection.connect(function(err) {
 })
 
 const supervisorMenu = () => {
+    let data,
+    output;
+
     inquirer
         .prompt([{
             name: 'superMenu',
@@ -25,5 +28,31 @@ const supervisorMenu = () => {
             message: "Supervisor Menu",
             choices: ["View Product Sales by Department", "Create New Department"]
         }])
-        .then
+        .then(({superMenu}) => {
+            connection.query(`SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));`, function(err,res) {
+                if (err) throw err
+            })
+            if (superMenu === "View Product Sales by Department") {
+            connection.query(`select departments.department_id, departments.department_name, departments.overhead_costs, products.product_sales
+            from departments
+            join products on departments.department_name = products.department_name
+            group by departments.department_id
+            `, function(err, res) {
+                if (err) throw err
+                let tableHeader = [["Department ID", "Department Name", "Overhead Costs", "Product Sales"]]
+                console.log(table(tableHeader))
+
+                for (let i = 0; i < res.length; i++){
+                data = [[res[i].department_id, res[i].department_name, res[i].overhead_costs, res[i].product_sales]]
+                  
+                output = table(data)
+                console.log(output)
+                }
+                
+            })
+            } else {
+                console.log("Create new apartment")
+            }
+            connection.end()
+        })
 }
